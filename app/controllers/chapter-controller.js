048ec685bbdee6a7bfb385851c9db8a7844bdc6c
nishant <nishant.singh87@gmail.com>
2015-08-06 00:21:04 +0530

    tech-debt: fixing console error(strange ui.router bug, added TODO in chapter-controller.js)

--- app/controllers/chapter-controller.js
+++ app/controllers/chapter-controller.js
@@ -9,11 +9,15 @@
             chapterName = function () {
               return $stateParams.chapter;
             },
+            bookName = function () {
+              return $stateParams.book;
+            },
             render = function (chapter) {
               $scope.chapter = chapter;
             };
 
         ApiService.getChapter(chapterName(), render, error);
+        $scope.book = bookName(); //TODO required for a bug in ui.router(or i dont get it?
       }]
   );
 
