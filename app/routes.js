a4dca2d6d7e5bd154224f6fcaadd2827496896a8
nishant <nishant.singh87@gmail.com>
2015-08-05 23:50:33 +0530

    making chapter a nested view of a book.

--- app/routes.js
+++ app/routes.js
@@ -1,19 +1,21 @@
 (function () {
   "use strict"
 
-  nglarily.module.config(["$stateProvider", function ($stateProvider) {
+  nglarily.module.config(["$stateProvider", "$urlRouterProvider", function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.when("", "/book");
+
     $stateProvider
-        .state("book-contents-page", {
-          url: "",
+        .state("book", {
+          url: "/book",
           templateUrl: "pages/book.html",
           controller: "BookController"
         })
-        .state("chapter", {
+        .state("book.chapter", {
           url: "/chapter/:chapter",
           templateUrl: "pages/chapter.html",
           controller: "ChapterController"
         })
-        .state("chapter.lesson", {
+        .state("book.chapter.lesson", {
           url: "/lesson/:lesson",               // creates nested route: chapter/:chapter/lesson/:lesson (because state is named chapter.lesson).
           templateUrl: "pages/lesson.html",
           controller: "LessonController"
