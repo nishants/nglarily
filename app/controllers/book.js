c5a567ad01934e924a157ca22a7939cadf75be74
nishant <nishant.singh87@gmail.com>
2015-08-14 00:20:54 +0530

    adding forward book navigation on right key press.

--- app/controllers/book.js
+++ app/controllers/book.js
@@ -7,14 +7,33 @@
             },
             error = function (err) {
               console.error(err);
+            },
+            goBack = function(){alert("lets go back...");},
+            goNext = function () {
+              if (!$scope.book) return; // ignore if book not loaded
+              window.location = BookNavigator.nextUrl($scope.book, {location: false});
             };
 
-        $scope.next = function(){
-          return $scope.book ? BookNavigator.nextUrl($scope.book): "";
+        $scope.next = function () {
+          // wait till book loads.
+          return $scope.book ? BookNavigator.nextUrl($scope.book) : "";
         };
 
-        $scope.collapseBook = function(){
+
+        $scope.collapseBook = function () {
           return UserView.chapterVisible();
+        };
+
+        document.onkeydown = function (evt) {
+          evt = evt || window.event;
+          switch (evt.keyCode) {
+            case 37:
+              goBack();
+              break;
+            case 39:
+              goNext();
+              break;
+          }
         };
 
         DataService.book(success, error);
