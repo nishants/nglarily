3187a50c6855514c375be281d9857aa53fd17538
nishant <nishant.singh87@gmail.com>
2015-08-13 20:23:30 +0530

    adding font face for source code.

--- app/controllers/sections/code-section.js
+++ app/controllers/sections/code-section.js
@@ -4,9 +4,17 @@
   nglarily.module.controller("CodeSection", ["$scope", "SectionData", "$timeout", function ($scope, SectionData, $timeout) {
         var
             editorOptions = {
+              //remove default red dots for unknown character(linefeed, etc)
+              specialCharPlaceholder: function(){return document.createElement("span")},
               readOnly: true,
-              lineNumbers: true,
+
+              // show line numbers in editor
+              //lineNumbers: true,
+
+              // renders whole document at once (else only part in view is rendered, and text searches wont work)
               viewportMargin: Infinity,
+
+              // syntax highlighter mode
               mode: "htmlmixed"
             },
 
