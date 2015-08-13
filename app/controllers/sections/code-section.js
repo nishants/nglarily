2c6ec5ccad409e9e59a33f6e6e1e5457460bc2c5
nishant <nishant.singh87@gmail.com>
2015-08-13 17:46:53 +0530

    use code mirror to render code in code sections.

--- app/controllers/sections/code-section.js
+++ app/controllers/sections/code-section.js
@@ -1,13 +1,28 @@
 (function () {
   "use strict"
 
-  nglarily.module.controller("CodeSection", ["$scope", "SectionData", function ($scope, SectionData) {
+  nglarily.module.controller("CodeSection", ["$scope", "SectionData", "$timeout", function ($scope, SectionData, $timeout) {
         var
+            editorOptions = {
+              readOnly: true
+            },
+
+            editorElement = function(){
+              return document.getElementById("code-editor")
+            },
+
             showError = function (err) {
               alert(err);
             },
+
+            setCodeMirror = function () {
+              CodeMirror.fromTextArea(editorElement(), editorOptions);
+            },
+
             setCode = function (code) {
               $scope.code = code;
+              $timeout(setCodeMirror);
+
             };
 
         SectionData.codeFor($scope.section, setCode, showError);
