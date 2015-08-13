392aabe264ee1b778d359d603441df0e72ac9161
nishant <nishant.singh87@gmail.com>
2015-08-13 22:53:07 +0530

    open next leson of next chapter on next.

--- app/models/chapter.js
+++ app/models/chapter.js
@@ -1,9 +1,23 @@
 (function () {
   "use strict"
 
-  var Chapter = function (data, lessons) {
-    this.name     = data.name;
-    this.lessons  = lessons;
+  var
+      indexOf = function (lessonName, lessons) {
+        for (var i = 0; i < lessons.length; i++) {
+          if (lessons[i].name == lessonName) {
+            return i;
+          }
+        }
+        return -1;
+      },
+
+      Chapter = function (data, lessons) {
+        this.name = data.name;
+        this.lessons = lessons;
+      };
+
+  Chapter.prototype.nextTo = function (lessonName) {
+    return this.lessons[indexOf(lessonName, this.lessons) +1];
   };
 
   nglarily.models.Chapter = Chapter;
--- app/services/book-navigator.js
+++ app/services/book-navigator.js
@@ -6,12 +6,24 @@
     var openChapter = function (chapter) {
           $state.go("book.chapter", {
             chapter: chapter.name
-          })
+          });
         },
 
+        openLesson = function (chapter, lesson) {
+          $state.go("book.chapter.lesson", {
+            chapter: chapter.name,
+            lesson: lesson.name
+          });
+        },
         goNext = function (book) {
-          var nextChapter = book.nextTo(UserView.currentChapter());
-          if (nextChapter) {
+          var
+              currentLesson = book.nextTo(UserView.currentLesson()),
+              nextChapter = book.nextTo(UserView.currentChapter()),
+              nextLesson = nextChapter ? nextChapter.nextTo(currentLesson) : undefined;
+
+          if (currentLesson) {
+            openLesson(nextChapter, nextLesson);
+          } else if (nextChapter) {
             openChapter(nextChapter);
           }
         };
