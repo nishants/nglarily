bf9b2f5be07395d4fd6e11d2dcb0a56b2c37aee0
nishant <nishant.singh87@gmail.com>
2015-08-18 00:36:18 +0530

    renaming snippet to section.

--- app/models/section.js
+++ app/models/section.js
@@ -2,7 +2,7 @@
   "use strict"
 
   var
-      Snippet = function (code, desc) {
+      Section = function (code, desc) {
         this.code = code;
         this.desc = desc;
       },
@@ -11,9 +11,9 @@
         var tokens      = snipppetText.split("**##"),
             code        = tokens[0],
             description = nglarily.models.SnippetDescription(tokens[1]);
-        return new Snippet(code, description);
+        return new Section(code, description);
       };
 
-  nglarily.models.Snippet = parse;
+  nglarily.models.Section = parse;
 
 }).call(this);
