90f54815d2c4ff0f74c796e5e5b12089d1e2580e
nishant <nishant.singh87@gmail.com>
2015-08-17 21:32:07 +0530

    parse snippet description from sourc code.

--- app/models/snippet-description.js
+++ app/models/snippet-description.js
@@ -2,12 +2,27 @@
   "use strict"
 
   var
-      SnippetDescription = function (text) {
-        this.text = text;
+      SnippetDescription = function (head, lines) {
+        this.head = head;
+        this.lines = lines;
       },
 
-      parse = function(text){
-        return new SnippetDescription(text)
+      parseLines = function (text) {
+        var parsed = [],
+            textLines = text.split("#li.");
+
+        for (var i = 0; i < textLines.length; i++) {
+          if(textLines[i].indexOf("#h1.") == -1){
+            parsed.push(textLines[i]);
+          };
+        }
+        return parsed;
+      },
+      parse = function (text) {
+        var
+            head = text.split("#li.")[0].replace("#h1.", "").replace("\n", "").trim(),
+            lines = parseLines(text);
+        return new SnippetDescription(head, lines)
       };
 
 
