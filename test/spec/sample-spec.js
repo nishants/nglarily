4a647168266f1b72372c30304f9b313e4d41fe5e
nishant <nishant.singh87@gmail.com>
2015-08-20 12:13:45 +0530

    adding jasmine

---
+++ test/spec/sample-spec.js
@@ -0,0 +1,14 @@
+describe("sample spe", function () {
+
+  var
+      setupAngular = function () {};
+
+  beforeEach(setupAngular);
+
+  // Tests
+  describe('test setup', function () {
+    it('should run a spec', function () {
+      expect(1).toBe(1);
+    });
+  });
+});
